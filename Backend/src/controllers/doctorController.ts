2d71d9d49b5d2672a28eaadc61a5d948e1ef8649
ADJs Codes <adjscode@gmail.com>
2026-03-27 12:06:47 +0100

    fixes: fixing node mailers connection

--- Backend/src/controllers/doctorController.ts
+++ Backend/src/controllers/doctorController.ts
@@ -218,6 +218,7 @@ const updateDoctorProfile = async (req: Request, res: Response) => {
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
