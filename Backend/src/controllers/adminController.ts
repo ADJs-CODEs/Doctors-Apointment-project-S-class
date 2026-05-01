bc26a75e02bae7ebe6efb1528abec36544fc6b45
ADJs Codes <adjscode@gmail.com>
2026-05-01 20:36:36 +0100

    feat: breaking down code for easier access and readability ------ fixed wrong status code error, typo error

--- Backend/src/controllers/adminController.ts
+++ Backend/src/controllers/adminController.ts
@@ -96,7 +96,7 @@ const loginAdmin = async (req: Request, res: Response) => {
 const allDoctors = async (req: Request, res: Response) => {
   try {
     const doctors = await doctorModel.find({}).select('-password')
-    res.status(400).json({ success: true, doctors })
+    res.status(200).json({ success: true, doctors })
   } catch (error: any) {
     console.log(error)
     res.status(500).json({ success: false, message: error.message })
